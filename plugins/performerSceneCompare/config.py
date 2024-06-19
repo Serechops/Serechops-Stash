6743217ef889a94973463e3accb7308a9064ef47
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-06-19 08:13:55 +0300

    Format config.py automatically

--- plugins/performerSceneCompare/config.py
+++ plugins/performerSceneCompare/config.py
@@ -1,10 +1,10 @@
 # config.py
 
 LOCAL_GQL_ENDPOINT = "http://localhost:9999/graphql"
-LOCAL_API_KEY = "" # Leave blank if you don't have credentials on your Stash.
+LOCAL_API_KEY = ""  # Leave blank if you don't have credentials on your Stash.
 
 MISSING_GQL_ENDPOINT = "http://localhost:6969/graphql"
-MISSING_API_KEY = "" # Leave blank if you don't have credentials on your Stash.
+MISSING_API_KEY = ""  # Leave blank if you don't have credentials on your Stash.
 
 STASHDB_ENDPOINT = "https://stashdb.org/graphql"
 STASHDB_API_KEY = ""
