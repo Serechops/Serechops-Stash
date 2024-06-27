4a39cedf15ee685d4ca67fc5fd14990e1e8b224f
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-06-27 05:55:15 +0300

    Remove GQL_ENDPOINT config variables
    These were needed before switch to stashapp-tools.

--- plugins/performerSceneCompare/config.py
+++ plugins/performerSceneCompare/config.py
@@ -3,13 +3,11 @@
 LOCAL_GQL_SCHEME = "http"
 LOCAL_GQL_HOST = "localhost"
 LOCAL_GQL_PORT = 9999
-LOCAL_GQL_ENDPOINT = "http://localhost:9999/graphql"
 LOCAL_API_KEY = ""  # Leave blank if you don't have credentials on your Stash.
 
 MISSING_GQL_SCHEME = "http"
 MISSING_GQL_HOST = "localhost"
 MISSING_GQL_PORT = 6969
-MISSING_GQL_ENDPOINT = "http://localhost:6969/graphql"
 MISSING_API_KEY = ""  # Leave blank if you don't have credentials on your Stash.
 
 STASHDB_ENDPOINT = "https://stashdb.org/graphql"
