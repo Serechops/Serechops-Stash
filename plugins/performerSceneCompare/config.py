a4013b2c467d6c80ac873eb854d41f96d2dc2e34
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-06-27 06:13:04 +0300

    Remove revoked StashDB API key
    Who idiot commits these in Git anyway...

--- plugins/performerSceneCompare/config.py
+++ plugins/performerSceneCompare/config.py
@@ -14,21 +14,3 @@ STASHDB_ENDPOINT = "https://stashdb.org/graphql"
 STASHDB_API_KEY = ""
 
 EXCLUDE_TAGS = []  # Scenes containing any of these tags will be excluded.
-
-
-LOCAL_GQL_SCHEME = "http"
-LOCAL_GQL_HOST = "localhost"
-LOCAL_GQL_PORT = 9999
-LOCAL_GQL_ENDPOINT = "http://localhost:9999/graphql"
-LOCAL_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJzdGFzaCIsInN1YiI6IkFQSUtleSIsImlhdCI6MTcwOTU0Mzk2Mn0.oQxeczmukk1u-gyfB-S_ucJ-OMYiWtx8pIbU7XdQrdg"  # Leave blank if you don't have credentials on your Stash.
-
-MISSING_GQL_SCHEME = "http"
-MISSING_GQL_HOST = "localhost"
-MISSING_GQL_PORT = 6969
-MISSING_GQL_ENDPOINT = "http://localhost:6969/graphql"
-MISSING_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJzdGFzaCIsInN1YiI6IkFQSUtleSIsImlhdCI6MTcxODE2MjIwMX0.55itivc7LfOl-KKo_SUBKjSIwQCXn-26aavN-BYxqxc"
-
-STASHDB_ENDPOINT = "https://stashdb.org/graphql"
-STASHDB_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI3ZDc4MWZkNy1hZGIwLTQ5ZDEtOWU3Ni1lMDJjYmQ0ODViNmUiLCJzdWIiOiJBUElLZXkiLCJpYXQiOjE2OTkwOTU3MDN9.acR6aAUcmbNTctQchM6XJioPjZA40qHtm0NoUb4h0PA"
-
-EXCLUDE_TAGS = ["Compilation"]
